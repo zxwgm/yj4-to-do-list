005f3a17025b98a3ee9f78a83e21af6c9f965e33
zxwgm <jeff0630@naver.com>
2023-11-08 13:22:51 +0900

    java

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello js")
+// 변수 
+const whoAmI = "김지승"
+let hobby = "달리기"
+
+const a = "10"
+const b = 20
+
+console.log(typeof whoAmI)
+console.log(typeof a)
+ 
+// ""
+// ''
+// ``
+
+//console.log("나의 이름은" + WhoAmI + "이고, 나의 취미는" + hobby + "입니다")
+//console.log(`나의 이름은 ${WhoAmI} 이고, 나의 취미는 ${hobby} 입니다`)
+
+const arr = [1, 2, 3]
+console.log(arr[1])
